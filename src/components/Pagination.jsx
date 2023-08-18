e50011920538259c16c239dd93908d0f09fbd14f
samaslima <samaslimag@gmail.com>
2023-08-18 17:44:22 -0300

    add pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,60 @@
+import { ChevronLeftIcon, ArrowLeftIcon, ChevronRightIcon, ArrowRightIcon } from '@chakra-ui/icons'
+import { Flex, IconButton, Tooltip } from '@chakra-ui/react'
+
+export default function Pagination(props) {
+
+    const pageNumbers = []
+
+    for (let i = 1; i <= Math.ceil(props.items/props.itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <>
+            <Flex m={4} justifyContent='center' gap='35px'>
+                <Flex>
+                    <Tooltip label='First Page'>
+                        <IconButton
+                            bg='white'
+                            icon={<ArrowLeftIcon h={3} w={3} />}
+                            mr={4}
+                        />
+                    </Tooltip>
+                    <Tooltip label='Previous Page'>
+                        <IconButton
+                            bg='white'
+                            icon={<ChevronLeftIcon h={6} w={6} />}
+                        />
+                    </Tooltip>
+                </Flex>
+                
+                {pageNumbers.map(page => 
+                    <a 
+                        key={page}
+                        onClick={(e) => {
+                            e.preventDefault()}}
+                    >
+                            {page}
+                    </a>
+                
+                )}
+
+                <Flex>
+                    <Tooltip label='Next Page'>
+                        <IconButton
+                            bg='white'
+                            icon={<ChevronRightIcon h={6} w={6} />}
+                            mr={4}
+                        />
+                    </Tooltip>
+                    <Tooltip label='Last Page'>
+                        <IconButton
+                            bg='white'
+                            icon={<ArrowRightIcon h={3} w={3} />}
+                        />
+                    </Tooltip>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
--- src/components/match/TableComponent.jsx
+++ src/components/match/TableComponent.jsx
@@ -9,8 +9,7 @@ import {
     Tooltip,
     Table, 
     Thead, 
-    Tbody, 
-    Tfoot,
+    Tbody,
     Tr, 
     Th, 
     Td, 
@@ -19,21 +18,42 @@ import {
     useDisclosure
 } from '@chakra-ui/react'
 import { EditIcon, DeleteIcon, SearchIcon, AddIcon } from '@chakra-ui/icons'
-import UpdateMatch from './UpdateMatch'
 import { Link } from 'react-router-dom'
+import UpdateMatch from './UpdateMatch'
+import Pagination from '../Pagination'
 
 export default function TableComponent() {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const matches = [
-        { id: 1, game: 'Fungi', players: 'João, Maria', winner: 'Maria' },
-        { id: 2, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas' },
-        { id: 3, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda' },
-        { id: 1, game: 'Fungi', players: 'João, Maria', winner: 'Maria' },
-        { id: 2, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas' },
-        { id: 3, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda' }
+    const itemsPerPage = 6
+
+    const gameMatches = [
+        { id: 1, game: 'Fungi', players: 'João, Maria', winner: 'Maria', date: '11/02/2023' },
+        { id: 2, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas', date: '10/05/2023' },
+        { id: 3, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '20/06/2023' },
+        { id: 4, game: 'Fungi', players: 'João, Maria', winner: 'Maria', date: '01/01/2023' },
+        { id: 5, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas', date: '24/02/2023' },
+        { id: 6, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '19/02/2023' },
+        { id: 7, game: 'Fungi', players: 'João, Maria', winner: 'Maria', date: '11/02/2023' },
+        { id: 8, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas', date: '10/05/2023' },
+        { id: 9, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '20/06/2023' },
+        { id: 10, game: 'Fungi', players: 'João, Maria', winner: 'Maria', date: '01/01/2023' },
+        { id: 11, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas', date: '24/02/2023' },
+        { id: 12, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '19/02/2023' }
     ]
+
+    var matches = []
+
+    let i = 0
+
+    for (let match of gameMatches) {
+        if (i < itemsPerPage) {
+            matches.push(match)
+        }
+
+        i++
+    }
 
     return (
         <>
@@ -69,6 +89,7 @@ export default function TableComponent() {
                                 <Th>Game</Th>
                                 <Th>Players</Th>
                                 <Th>Winner</Th>
+                                <Th>Date</Th>
                             </Tr>
                         </Thead>
                         <Tbody>
@@ -79,6 +100,7 @@ export default function TableComponent() {
                                         <Td>{ match.game }</Td>
                                         <Td>{ match.players }</Td>
                                         <Td>{ match.winner }</Td>
+                                        <Td>{ match.date }</Td>
                                         <Td>
                                             <Tooltip label='Update'>
                                                 <IconButton
@@ -102,6 +124,8 @@ export default function TableComponent() {
                         </Tbody>
                     </Table>
                 </TableContainer>
+
+                <Pagination items={gameMatches.length} itemsPerPage={itemsPerPage} />
             </Box>
 
             <UpdateMatch openModal={isOpen} closeModal={onClose} />
