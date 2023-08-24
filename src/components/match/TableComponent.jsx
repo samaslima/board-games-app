46fa02707fd56a3e7d5edfd7d3b2d1a7cfa29ab3
samaslima <samaslimag@gmail.com>
2023-08-24 15:42:39 -0300

    fix search input

--- src/components/match/TableComponent.jsx
+++ src/components/match/TableComponent.jsx
@@ -1,5 +1,14 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 import { 
     Box,
+    Button,
+    Flex,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    Heading,
     IconButton,
     Tooltip,
     Table, 
@@ -11,16 +20,26 @@ import {
     TableContainer,
     useDisclosure
 } from '@chakra-ui/react'
-import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
+
+import { 
+    AddIcon, 
+    EditIcon, 
+    DeleteIcon, 
+    SearchIcon 
+} from '@chakra-ui/icons'
+
 import Pagination from '../Pagination'
-import HeaderTable from './HeaderTable'
 import Dialog from '../Dialog'
 import ModalMatch from '../ModalMatch'
 
 export default function TableComponent() {
 
+    const { isOpen: isOpenAdd, onOpen: onOpenAdd, onClose: onCloseAdd } = useDisclosure()
     const { isOpen: isOpenUpdate, onOpen: onOpenUpdate, onClose: onCloseUpdate } = useDisclosure()
     const { isOpen: isOpenDelete, onOpen: onOpenDelete, onClose: onCloseDelete } = useDisclosure()
+
+    const [searchMatch, setSearchMatch] = useState('')
+    const [filteredList, setFilteredList] = useState('')
 
     const itemsPerPage = 6
 
@@ -30,30 +49,59 @@ export default function TableComponent() {
         { id: 3, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '20/06/2023' },
         { id: 4, game: 'Honeydukes', players: 'João, Maria, Pedro', winner: 'Pedro', date: '01/06/2023' },
         { id: 5, game: 'Dixit', players: 'Maria, Clara, Lucas, Miguel', winner: 'Clara', date: '24/06/2023' },
-        { id: 6, game: '7 Wonders', players: 'Luiza, Tereza', winner: 'Luiza', date: '21/02/2023' },
-        { id: 7, game: 'Fungi', players: 'João, Maria', winner: 'Maria', date: '11/02/2023' },
-        { id: 8, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas', date: '10/05/2023' },
-        { id: 9, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '20/06/2023' },
-        { id: 10, game: 'Fungi', players: 'João, Maria', winner: 'Maria', date: '01/01/2023' },
-        { id: 11, game: 'War', players: 'Maria, Clara, Lucas, Miguel', winner: 'Lucas', date: '24/02/2023' },
-        { id: 12, game: 'Claim', players: 'Júlia, Amanda', winner: 'Amanda', date: '19/02/2023' }
+        { id: 6, game: '7 Wonders', players: 'Luiza, Tereza', winner: 'Luiza', date: '21/02/2023' }
     ]
 
-    var matches = []
+    const searchMatches = (value) => {
+        setSearchMatch(value)
 
-    let i = 0
+        if (searchMatch !== '') {
+            const filterList = gameMatches.filter((item) => {
+                return Object.values(item).join('').toLowerCase().includes(searchMatch.toLowerCase())
+            })
 
-    for (let match of gameMatches) {
-        if (i < itemsPerPage) {
-            matches.push(match)
+            setFilteredList(filterList)
+        } else {
+            setFilteredList(gameMatches)
         }
-
-        i++
     }
 
     return (
         <>
-            <HeaderTable />
+            <Box w='full' px={5} py={8} shadow='md' mx='auto'>
+                <Flex alignItems='center' justify='space-between'>
+                    <Flex>
+                        <Link to='/'>
+                            <Heading as='h4' size='md'>
+                                BOARD GAMES
+                            </Heading>
+                        </Link>
+                    </Flex>
+                    <Flex>
+                        <InputGroup>
+                            <InputLeftElement pointerEvents='none'>
+                                <SearchIcon color='gray.300' />
+                            </InputLeftElement>
+                            <Input 
+                                variant='filled' 
+                                placeholder='Search' 
+                                w='350px'
+                                onChange={(e) => searchMatches(e.target.value)} 
+                            />
+                        </InputGroup>
+                    </Flex>
+                    <Flex>
+                        <Button 
+                            leftIcon={<AddIcon />} 
+                            colorScheme='green' 
+                            variant='solid' 
+                            onClick={onOpenAdd}>
+                                ADD NEW MATCH
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Box>
+
             <Box padding='2em' borderRadius='lg' borderWidth='1px'>
                 <TableContainer paddingTop='1em'>
                     <Table variant='simple' size='lg'>
@@ -66,35 +114,66 @@ export default function TableComponent() {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {
-                                matches.map(
-                                    match =>
-                                    <Tr key= { match.id }>
-                                        <Td>{ match.game }</Td>
-                                        <Td>{ match.players }</Td>
-                                        <Td>{ match.winner }</Td>
-                                        <Td>{ match.date }</Td>
-                                        <Td>
-                                            <Tooltip label='Update'>
-                                                <IconButton
-                                                    aria-label='Edit'
-                                                    mr={6}
-                                                    icon={<EditIcon />}
-                                                    onClick={onOpenUpdate}
-                                                />
-                                            </Tooltip>
-                                            <Tooltip label='Delete'>
-                                                <IconButton
-                                                    aria-label='Delete'
-                                                    icon={<DeleteIcon />}
-                                                    colorScheme='red'
-                                                    onClick={onOpenDelete}
-                                                />
-                                            </Tooltip>
-                                        </Td>
-                                    </Tr>
+
+                            {searchMatch.length > 1 ? (
+                                filteredList.map((match) => {
+                                    return (
+                                        <Tr key= { match.id }>
+                                            <Td>{ match.game }</Td>
+                                            <Td>{ match.players }</Td>
+                                            <Td>{ match.winner }</Td>
+                                            <Td>{ match.date }</Td>
+                                            <Td>
+                                                <Tooltip label='Update'>
+                                                    <IconButton
+                                                        aria-label='Edit'
+                                                        mr={6}
+                                                        icon={<EditIcon />}
+                                                        onClick={onOpenUpdate}
+                                                    />
+                                                </Tooltip>
+                                                <Tooltip label='Delete'>
+                                                    <IconButton
+                                                        aria-label='Delete'
+                                                        icon={<DeleteIcon />}
+                                                        colorScheme='red'
+                                                        onClick={onOpenDelete}
+                                                    />
+                                                </Tooltip>
+                                            </Td>
+                                        </Tr>
                                     )
-                            }
+                                })
+                            ) : (
+                                gameMatches.map((match) => {
+                                    return (
+                                        <Tr key= { match.id }>
+                                            <Td>{ match.game }</Td>
+                                            <Td>{ match.players }</Td>
+                                            <Td>{ match.winner }</Td>
+                                            <Td>{ match.date }</Td>
+                                            <Td>
+                                                <Tooltip label='Update'>
+                                                    <IconButton
+                                                        aria-label='Edit'
+                                                        mr={6}
+                                                        icon={<EditIcon />}
+                                                        onClick={onOpenUpdate}
+                                                    />
+                                                </Tooltip>
+                                                <Tooltip label='Delete'>
+                                                    <IconButton
+                                                        aria-label='Delete'
+                                                        icon={<DeleteIcon />}
+                                                        colorScheme='red'
+                                                        onClick={onOpenDelete}
+                                                    />
+                                                </Tooltip>
+                                            </Td>
+                                        </Tr>
+                                    )
+                                })
+                            )}
                         </Tbody>
                     </Table>
                 </TableContainer>
@@ -103,11 +182,19 @@ export default function TableComponent() {
             </Box>
 
             <ModalMatch 
+                openModal={isOpenAdd} 
+                closeModal={onCloseAdd} 
+                modalHeader={`Register new match`}
+                nameButton={`Save`} 
+            />
+
+            <ModalMatch 
                 openModal={isOpenUpdate} 
                 closeModal={onCloseUpdate} 
                 modalHeader={`Update match`}
                 nameButton={`Update`} 
             />
+
             <Dialog 
                 openModal={isOpenDelete}
                 closeModal={onCloseDelete}
